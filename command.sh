6dcbfa5179bf1278932d94851264730e1494aca0
richardathitparade <richard@hitparade.co>
2017-11-17 14:43:17 -0500

    updates

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/vSAyeqP9VZAavneCm9VLEh' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.tornado_settings="{'headers': {'Content-Security-Policy': \"frame-ancestors 'self' www.floydhub.com \"}}" --NotebookApp.iopub_data_rate_limit=1.0e10
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/M6KfJutBH3yJ6BvEUDER2J' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.tornado_settings="{'headers': {'Content-Security-Policy': \"frame-ancestors 'self' www.floydhub.com \"}}" --NotebookApp.iopub_data_rate_limit=1.0e10
